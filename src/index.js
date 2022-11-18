2729a77766be74da6efdc92c9c0662261ce020d6
magichano <antonartamonov1@gmail.com>
2022-11-19 00:13:08 +0400

    task is done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function reverse (n) {
-  
+
+    
+    var result='';
+
+    if (n>0) {
+        n=n.toString();
+    }
+    else{
+        n=-n;
+        n=n.toString();
+    };
+
+  for (i=n.length-1; i>=0; i=i-1){
+    result=result + n[i];
+   // console.log(n[i]);
+    console.log(result);
+  }
+
+  return Number(result); 
 }
+
+//reverse(-425);
